11a386def857d2fde1f01506f510b571899ea960
muhammed fazil <muhammed.fazil@neopraxis.in>
2024-02-14 17:38:45 +0530

    data recive

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const bodyParser = require('body-parser'); // Added for parsing request body
+
 var request = require('request');
 require('dotenv').config();
 const app = express();
@@ -14,47 +15,16 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const port = process.env.PORT;
+
 const store_hash = process.env.STORE_HASH;
+
 const access_token = process.env.ACCESS_TOKEN;
+
 const django_endpoint_baseurl = process.env.DJANGO_ENDPOINT_BASE_URL;
 
 // Mock user credentials for demonstration purposes
-const validCredentials = {
-  correctStoreHash: '',
-  correctStoreToken: '',
-};
+const validCredentials = {correctStoreHash: '',correctStoreToken: '',};
 
-app.post('/api/login', async (req, res) => {
-  const { storeHash, storeToken } = req.body;
-  // Validate user credentials with BigCommerce API
-  try {
-    const url = `https://api.bigcommerce.com/stores/${storeHash}/v2/store`;
-    const config = {
-      method: 'get',
-      url: url,
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'X-Auth-Token': storeToken,
-      },
-    };
-
-    const response = await axios(config);
-
-    if (response.status === 200) {
-      // Successful validation
-      // console.log("Validate completed");
-      res.json({ message: 'Store validation successful' });
-    } else {
-      // Failed validation
-      // console.log("validation failed");
-      res.status(response.status).json({ message: 'Store validation failed' });
-    }
-  } catch (error) {
-    console.error('Error during store validation:', error);
-    res.status(500).json({ message: 'Error during store validation' });
-  }
-});
 
 app.get('/api/data', async (req, res) => {
   const url = `https://api.bigcommerce.com/stores/${store_hash}/v3/catalog/products`;
@@ -100,87 +70,30 @@ app.listen(port, () => {
 });
 
 
-let sessionData = {}; // Define a variable to store session data
+var userData = {}
+
 
 // Get the current user session id
 app.post('/get_current_sid', async (req, res) => {
   const s_id = req.body.current_id;  
-  const token = req.body.current_token;  
-
-  console.log(`
-        sid is here ${s_id}
-        token is here ${token}
-  `);
-
-  // Store session data
-  // sessionData = { session_id: s_id, token: token };
- 
-  // console.log(sessionData)
- 
-  const userData = await validateSession(s_id,token)
+  const token = req.body.current_token;   
+  userData = await validateSession(s_id,token)
   console.log('<<<<<<< USER DATA IS HERE 555 >>>>>>>>>>',userData)
- 
   res.json({ message: 'Session ID received and validated successfully.',userData});
 }); 
 
 
-// Define route handler for GET request to '/uuid
-// app.get('/uuid', async (req, res) => {
-//   const url = `http://127.0.0.1:8000/get-data`;
-
-//   let config = {
-//     method: 'get',
-//     url: url,
-//     headers: {
-//       Accept: 'application/json',
-
-      
-//     }
-//   };
-
-//   try {
-//     const response = await axios(config);
-
-//     console.log('what is my response here',response)
-
-//     const session_id = response.data.session_id;
-//     const token = response.data.token;
-
-
-//     console.log(session_id,"session id is here");
-//     console.log(token,"token  is here");
-
-//   } catch (err) {
-//     console.error('error:', err);
-//     res.status(500).json({ message: 'Error fetching data' });
-//   }
-// });
-
-
-
 // Validation function (same as in Endpoint 1)
 async function validateSession(s_id, token) {
-
-  console.log('in validate session is here',s_id,token)
-  
-
   try {
       var sid = s_id
-
-      console.log('what is my sid here',sid);
-
       url = `${django_endpoint_baseurl}/api/authuser?sessionkey=${sid}`
-
-      console.log('what is my url for the call',url);
-
-
       if(token){
         const options = {
           method: 'GET',
           url:url,
           headers: {Authorization: `Token ${token}`}
         };
-  
         const response =  await axios(options);
         return response.data;
       }
@@ -188,12 +101,51 @@ async function validateSession(s_id, token) {
       console.error('Error validating session with Django:', error);
       throw error;
     }
-
   }
 
 
 
+  app.post('/api/login', async (req, res) => {
 
+    console.log(`what is login user data ................`,userData.email)
+
+
+
+    const { storeHash, storeToken } = req.body;
+    
+    console.log(`what is my storehash ${storeHash} and token is ${storeToken}`)
+
+    // Validate user credentials with BigCommerce API
+    try {
+      const url = `https://api.bigcommerce.com/stores/${storeHash}/v2/store`;
+    
+      const config = {
+        method: 'get',
+        url: url,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'X-Auth-Token': storeToken,
+        },
+      };
+  
+      const response = await axios(config);
+  
+      if (response.status === 200) {
+        // Successful validation
+        // console.log("Validate completed");
+        res.json({ message: 'Store validation successful' });
+      } else {
+        // Failed validation
+        // console.log("validation failed");
+        res.status(response.status).json({ message: 'Store validation failed' });
+      }
+    } catch (error) {
+      console.error('Error during store validation:', error);
+      res.status(500).json({ message: 'Error during store validation' });
+    }
+  });
+  
 
 
 
