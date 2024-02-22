c1cf3abb5880dcbaf60bb305b5b060ce7a32f6b6
muhammed fazil <muhammed.fazil@neopraxis.in>
2024-02-22 19:02:30 +0530

    working auth process

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const axios = require('axios');
 const cors = require('cors');
 const bodyParser = require('body-parser'); // Added for parsing request body
 
+
+
 var request = require('request');
 require('dotenv').config();
 const app = express();
@@ -17,6 +19,10 @@ require("./conn/conn")
 
 // Decode jwt token
 const jwt = require('jsonwebtoken');
+
+
+// Store in session data for temporily
+const session = require('express-session');
 
 
 // Enable CORS for all routes
@@ -38,6 +44,20 @@ const validCredentials = { correctStoreHash: '', correctStoreToken: '', };
 
 
 const UserData = require("./models/userdata")
+
+
+
+// Credentials store 
+const sessionSecretKey = 'x1231xxcvsdfSdfAdfdsfsxfsdkaL22sdadsxd1za';
+
+
+
+app.use(session({
+  secret: sessionSecretKey, // Change this to a long, randomly generated string
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 8 * 60 * 60 * 1000 } // Session expires after 8 hours
+}));
 
 
 
@@ -133,12 +153,12 @@ async function decodeJwt(token) {
 }
 
 
-async function generateJwtToken(uid){
+async function generateJwtToken(sid,uid){
   const secretKey = 'your_secret_key';
 
   const u_data = {
-    id: uid,
-    status: 'logged'
+    id: sid,
+    uid:uid
   };
   
   // Generate the JWT token with the user payload and secret key
@@ -147,6 +167,7 @@ async function generateJwtToken(uid){
 }
 
 async function checkStoreExists(storeHash, storeToken) {
+  console.log('inside the store check 8888888888888',storeHash,storeToken)
   try {
     const url = `https://api.bigcommerce.com/stores/${storeHash}/v2/store`;
     const config = {
@@ -160,66 +181,168 @@ async function checkStoreExists(storeHash, storeToken) {
     };
     const response = await axios(config);
 
+
     var storeData = {}
     if (response.data) {
       const store_response_data = response.data
       storeData = {
-        'store_status': 'active',
         'storeId': store_response_data.id,
         'storeName': store_response_data.name
       }
     }
+
+    console.log('what is in the storeData',storeData)
+
     return storeData;
   } catch (error) {
     console.error('Error faced with store validation')
   }
 }
 
-app.post('/api/login', async (req, res) => {
-  const { storeHash, storeToken, jwt_token } = req.body;
-  const decodedData = await decodeJwt(jwt_token);
+async function authenticate_user_with_jwt_token(req){
+  console.log('===============>',req)
+  console.log('MY jwt token is here =======>',req.session.jwtToken)
+  try {
+    url = `${django_endpoint_baseurl}/api/validatejwt`
+    const x_auth_token = 'svrleubeveruvbwekcnerunrevnbfvbfvjbsjkvbdvhfbvfhvfjserf'
+        
+    const options = {
+      method: 'POST',
+      url: url,
+      headers: {
+        'X-Auth-Token': `Bearer ${x_auth_token}`,
+        'Content-Type': 'application/json' // Specify the content type of the body
+      },
+      data: {
+        jwt_token: req.session.jwtToken
+      }
+    };
+
+    const response = await axios(options);    
+
+    return response.data
+
+  } catch (error) {
+    console.log(`You don't have autherazation token`)
+  }
+
+
+}
+
+app.post('/api/login', async (req,res) => {
   
-  var storeExists = '';
+  const { storeHash, storeToken, jwt_token } = req.body;
 
-  console.log('what is decoded data ===>', decodedData)
-  if (decodedData) {
-    const { useremail, userid, have_access_for_product_tool } = decodedData.user;
-    storeExists = await checkStoreExists(storeHash, storeToken)
+  req.session.storeHash = storeHash;
+  req.session.storeToken = storeToken;
+  req.session.jwtToken = jwt_token;
+  
+  // checks whether the jwt token is valid or not 
+  const  validate_verified_token =  await authenticate_user_with_jwt_token(req)
 
-    if (storeExists && storeExists.storeId) {
-        if(have_access_for_product_tool == true){
-          const existingUser = await UserData.findOne({ $or: [{ userid }, { useremail }] });
+  if(validate_verified_token){
+    req.session.userData = validate_verified_token;
+
+    console.log('My unique session id',req.session.id)
+
+    // validate the store with storehash and token
+
+    storeExists = await checkStoreExists(req.session.storeHash, req.session.storeToken)
 
 
-          if(existingUser){
-            console.log('User with this ID or email already exists.');
-          }else{
-            console.log('createng a new user in db')
-            const users = new UserData({ userid, useremail, storeHash, storeToken })
-            await users.save();
-          }
+    // It will be the user id dynamically get from the session
+    uid = 1223;
 
-        
-          unique_logged_token = await generateJwtToken(userid)
-          
-          res.json({
-            message: 'User have access to the app and store is valid',
-            status: 200,
-            pendingstatus:true,
-            token:unique_logged_token
-          });
-        
+    console.log('what is my store status =========>',storeExists)
 
-        }
-    } else {
-       res.json({
-        message: 'User have no access to the app and store is valid',
-        status: 500,
-        pendingstatus:false
-       });
+    if(storeExists){
+      console.log('Store is there=======>')
+      unique_logged_token = await generateJwtToken(req.session.id,uid)
+      console.log('these is my unique_logged_token here 8888',unique_logged_token)
+
+      req.session.signedtoken =  unique_logged_token
+
+      ult_token = req.session.signedtoken
+
+      console.log('buddy session data ====',req.session)
+
+
+
+     res.json({
+            "ult": ult_token,
+      });
+
+    
+      
+    }else{
+      console.log('store not exists')
+
+      res.json({
+        "message": 'store not exists error',
+      });
     }
 
+
+  }else{
+    console.log('JWT TOKEN IS NOT A VALID TOKEN VERIFIED FROM DJANGO SIDE')
+    res.json({
+      "message": 'jwt token is not valid',
+    });
   }
+
+
+
+
+
+
+  // const decodedData = await decodeJwt(jwt_token);
+  
+  // var storeExists = '';
+
+  // console.log('what is decoded data ===>', decodedData)
+
+  // res.json({
+  //         message: 'Login data recieved',
+  // });
+  
+  // if (decodedData) {
+  //   const { useremail, userid, have_access_for_product_tool } = decodedData.user;
+  //   storeExists = await checkStoreExists(storeHash, storeToken)
+
+  //   if (storeExists && storeExists.storeId) {
+  //       if(have_access_for_product_tool == true){
+  //         const existingUser = await UserData.findOne({ $or: [{ userid }, { useremail }] });
+
+
+  //         if(existingUser){
+  //           console.log('User with this ID or email already exists.');
+  //         }else{
+  //           console.log('createng a new user in db')
+  //           const users = new UserData({ userid, useremail, storeHash, storeToken })
+  //           await users.save();
+  //         }
+
+        
+  //         unique_logged_token = await generateJwtToken(userid)
+          
+  //         res.json({
+  //           message: 'User have access to the app and store is valid',
+  //           status: 200,
+  //           pendingstatus:true,
+  //           token:unique_logged_token
+  //         });
+        
+
+  //       }
+  //   } else {
+  //      res.json({
+  //       message: 'User have no access to the app and store is valid',
+  //       status: 500,
+  //       pendingstatus:false
+  //      });
+  //   }
+
+  // }
 
 
  
